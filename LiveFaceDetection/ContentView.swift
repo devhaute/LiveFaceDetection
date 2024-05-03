4a67375d2fe2f1744611b68a4de367d84bb4ef1d
devhaute <devhaute@gmail.com>
2024-05-03 17:04:34 +0900

    얼굴 감지 기능 구현

--- LiveFaceDetection/ContentView.swift
+++ LiveFaceDetection/ContentView.swift
@@ -10,15 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            CameraViewControllerWrapper()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct CameraViewControllerWrapper: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> VideoCaptureViewController {
+        VideoCaptureViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: VideoCaptureViewController, context: Context) {
+        // Here you can pass data to the UIViewController if needed
+    }
 }
---
+++ LiveFaceDetection/VideoCaptureViewController.swift
@@ -0,0 +1,123 @@
+//
+//  VideoCaptureViewController.swift
+//  LiveFaceDetection
+//
+//  Created by chanho on 5/3/24.
+//
+
+import UIKit
+import AVFoundation
+import Vision
+
+class VideoCaptureViewController: UIViewController {
+    private var captureDevice: AVCaptureDevice?
+    private var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    private let captureSession = AVCaptureSession()
+    private let photoOutput = AVCapturePhotoOutput()
+    private let videoOutput = AVCaptureVideoDataOutput()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpCamera()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getCameraFrames()
+        captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        captureSession.stopRunning()
+    }
+    
+    private func setUpCamera() {
+        guard let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
+            print("Failed to get capture device for camera position: front")
+            return
+        }
+        
+        captureSession.beginConfiguration()
+        captureSession.sessionPreset = .hd1280x720
+        captureDevice = device
+        
+        if let captureDevice {
+            do {
+                try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+                
+                if captureSession.canAddOutput(photoOutput) {
+                    captureSession.addOutput(photoOutput)
+                }
+                
+            } catch {
+                print("error: \(error.localizedDescription)")
+            }
+            
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            previewLayer?.frame = UIScreen.main.bounds
+            previewLayer?.videoGravity = .resizeAspectFill
+            
+            if let previewLayer {
+                view.layer.addSublayer(previewLayer)
+            }
+            
+            captureSession.commitConfiguration()
+            
+            if let audioDevice = AVCaptureDevice.default(for: .audio) {
+                do {
+                    let audioInput = try AVCaptureDeviceInput(device: audioDevice)
+                    if captureSession.canAddInput(audioInput) {
+                        captureSession.addInput(audioInput)
+                    }
+                } catch {
+                    print("Error setting up audio input: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+}
+
+extension VideoCaptureViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    private func getCameraFrames() {
+        videoOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString): NSNumber(value: kCVPixelFormatType_32BGRA)] as [String: Any]
+        videoOutput.alwaysDiscardsLateVideoFrames = true
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera_frame_processing_queue"))
+        if !captureSession.outputs.contains(videoOutput) {
+            captureSession.addOutput(videoOutput)
+        }
+        guard let connection = videoOutput.connection(with: .video),
+              connection.isVideoOrientationSupported else {
+            return
+        }
+        
+        connection.videoOrientation = .portrait
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let frame = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        detectFace(image: frame)
+    }
+}
+
+extension VideoCaptureViewController {
+    private func detectFace(image: CVPixelBuffer) {
+        let detectCaptureQualityRequest = VNDetectFaceCaptureQualityRequest()
+        detectCaptureQualityRequest.revision = VNDetectFaceCaptureQualityRequestRevision2   // iOS 14.0+
+        
+        let handler = VNImageRequestHandler(cvPixelBuffer: image, orientation: .leftMirrored, options: [:])
+        do {
+            try handler.perform([detectCaptureQualityRequest])
+            
+            if let results = detectCaptureQualityRequest.results as? [VNFaceObservation] {
+                for face in results {
+                    print("Face capture quality score: \(face.faceCaptureQuality ?? 0)")
+                }
+            }
+        } catch {
+            print("Failed to perform detection: \(error.localizedDescription)")
+        }
+    }
+}
