04dd305e3527c4f5c902912f6a37b01014f42346
devhaute <devhaute@gmail.com>
2024-05-03 14:47:10 +0900

    초기 환경 세팅

---
+++ LiveFaceDetection/LiveFaceDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveFaceDetectionApp.swift
+//  LiveFaceDetection
+//
+//  Created by chanho on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LiveFaceDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
